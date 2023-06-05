62ba2969ce02b161ac5c4fd3317228ebf96618c5
VANOREK Yessica <yessica.vanorek@gmail.com>
2023-06-05 17:23:18 +0200

    Initial commit

---
+++ src/main/java/com/example/maitainancemonitor/MaitainanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.maitainancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaitainanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaitainanceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maitainancemonitor/MaitainanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maitainancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaitainanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
